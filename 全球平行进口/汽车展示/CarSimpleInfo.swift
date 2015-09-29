fb8833c7c144410324a2dacd15ff608f814c1fe3
WangZhuo2015 <280034816@qq.com>
2015-09-30 00:27:44 +0800

    项目创建
    项目创建

---
+++ 全球平行进口/汽车展示/CarSimpleInfo.swift
@@ -0,0 +1,18 @@
+//
+//  CarSimpleInfo.swift
+//  全球平行进口
+//
+//  Created by 王卓 on 15/9/30.
+//  Copyright © 2015年 538_iOS_Team. All rights reserved.
+//
+
+import Foundation
+class CarSimpleInfo{
+    var carImageURL:NSURL
+    var carName:String
+    
+    init(Img:String,carName:String) {
+        carImageURL = NSURL(fileURLWithPath: Img)
+        self.carName = carName
+    }
+}
