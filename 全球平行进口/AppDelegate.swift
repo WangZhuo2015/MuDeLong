ea467f34389b6c92578ad6f4d25e82c23d3e35f4
WangZhuo2015 <280034816@qq.com>
2015-10-09 18:48:50 +0800

    更新10.9
    更新10.9

--- 全球平行进口/AppDelegate.swift
+++ 全球平行进口/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        UINavigationBar.appearance().backgroundColor=UIColor.blueColor()
+        //UINavigationBar.appearance().translucent=false
+        
         // Override point for customization after application launch.
         return true
     }
